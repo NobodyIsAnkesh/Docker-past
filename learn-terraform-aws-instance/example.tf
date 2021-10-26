1f170e3eff10b731cd715fce398544b2ae9d46ba
NobodyIsAnkesh <36071910+NobodyIsAnkesh@users.noreply.github.com>
2021-10-26 16:18:17 +0530

    Add files via upload

---
+++ learn-terraform-aws-instance/example.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
+
+output "aaa" {
+  value = aws_instance.example.ami
+}
